b024d060c0bba3391a72e9425c09081264a3d2dd
maunducyrus123@gmail.com <maunducyrus123@gmail.com>
2024-11-11 19:32:53 +0300

    admin.py files added

--- DjangoApplication/application/admin.py
+++ DjangoApplication/application/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from application.models import Student
+
+admin.site.register(Student)
 # Register your models here.
--- DjangoApplication/application/models.py
+++ DjangoApplication/application/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Student(models.Model):
     name = models.CharField(max_length=20)
+    # registration = models.CharField(max_length=30)
     age = models.IntegerField()
     gender = models.IntegerField(max_length=20)
     course = models.CharField(max_length=20)
